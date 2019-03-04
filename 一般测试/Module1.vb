fab2352601ba8a626f9e13c76a4713d38036918b
西斯 阿克 <2526681@qq.com>
2019-03-04 11:18:33 +0800

    测试更改

---
+++ 一般测试/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Text.RegularExpressions
+Module Module1
+
+    Sub Main()
+        Dim a As New Regex("def?")
+        Console.WriteLine(a.Match("ddddecfffff"))
+        Console.ReadLine()
+
+    End Sub
+
+
+
+End Module
