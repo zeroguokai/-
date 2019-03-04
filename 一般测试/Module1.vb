c44c0d44490a10ec7143bf8b9665855166903a69
西斯 阿克 <2526681@qq.com>
2019-03-04 11:24:53 +0800

    第二次修改

--- 一般测试/Module1.vb
+++ 一般测试/Module1.vb
@@ -4,6 +4,7 @@ Module Module1
     Sub Main()
         Dim a As New Regex("def?")
         Console.WriteLine(a.Match("ddddecffffff"))
+        Console.WriteLine("第二次修改")
         Console.ReadLine()
 
     End Sub
